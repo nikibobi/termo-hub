aeff4d7842ad072d6b29872d5fa740b34af612a2
Borislav Kosharov <bosakmaw@gmail.com>
2017-07-06 00:34:12 +0300

    db migration changed reading default time from local to utc

---
+++ TermoHub/Migrations/20170705205545_TimeDefaultUtc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TermoHub.Migrations
+{
+    public partial class TimeDefaultUtc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>("Time", "Readings", defaultValueSql: "GETUTCDATE()");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>("Time", "Readings", defaultValueSql: "GETDATE()");
+        }
+    }
+}
