24e9185afb734afe5e68ec74d1b365ead800a410
Borislav Kosharov <bosakmaw@gmail.com>
2017-08-28 19:30:13 +0300

    added gulp and package.json config files

---
+++ TermoHub/gulpfile.js
@@ -0,0 +1,17 @@
+﻿/// <binding AfterBuild='default' Clean='clean' />
+
+var gulp = require('gulp');
+var del = require('del');
+
+var paths = {
+    scripts: ['scripts/**/*.js', 'scripts/**/*.ts', 'scripts/**/*.map'],
+    dest: 'wwwroot/js'
+};
+
+gulp.task('clean', function () {
+    return del([paths.dest]);
+});
+
+gulp.task('default', function () {
+    gulp.src(paths.scripts).pipe(gulp.dest(paths.dest));
+});
