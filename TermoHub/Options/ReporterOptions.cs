a9d05b03cc0b182c9f99f45a0638944f14a798cf
Borislav Kosharov <bosakmaw@gmail.com>
2017-09-01 23:35:17 +0300

    add on/off subject prefix to reporter options

--- TermoHub/Options/ReporterOptions.cs
+++ TermoHub/Options/ReporterOptions.cs
@@ -3,6 +3,8 @@
     public class ReporterOptions
     {
         public string Hostname { get; set; }
+        public string OnPrefix { get; set; }
+        public string OffPrefix { get; set; }
         public string Subject { get; set; }
     }
 }
