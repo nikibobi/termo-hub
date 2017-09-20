ea61976068726ad47eedaf5dea35a4d8811c9c1b
Borislav Kosharov <bosakmaw@gmail.com>
2017-09-21 00:33:34 +0300

    update csproj and Program to 2.0 version

--- TermoHub/Program.cs
+++ TermoHub/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace TermoHub
@@ -7,15 +7,13 @@ namespace TermoHub
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .UseUrls("http://0.0.0.0:5000")
                 .Build();
-
-            host.Run();
-        }
     }
 }
