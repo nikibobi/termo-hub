ba232c43c340b2e5a5f93650391583534c5fd6db
Borislav Kosharov <bosakmaw@gmail.com>
2017-09-21 00:36:48 +0300

    change LastValues to lazy load sensor values and use scoped service
    (cherry picked from commit 67210e66424840c641e9a8f382adb75cddb67910)
    (cherry picked from commit 499fbf968c551a315082e186fc2c0867c4dfed17)

--- TermoHub/Services/ILastValues.cs
+++ TermoHub/Services/ILastValues.cs
@@ -2,7 +2,6 @@
 {
     public interface ILastValues
     {
-        double GetDeviceLastValuesAverage(int deviceId);
         double GetSensorLastValue(int deviceId, int sensorId);
         void SetSensorLastValue(int deviceId, int sensorId, double value);
     }
--- TermoHub/Services/LastValues.cs
+++ TermoHub/Services/LastValues.cs
@@ -1,58 +1,45 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Concurrent;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using TermoHub.Models;
 
 namespace TermoHub.Services
 {
     public class LastValues : ILastValues
     {
-        private readonly IDictionary<int, IDictionary<int, double>> cache;
+        private readonly IDictionary<(int, int), double> cache;
+        private readonly IServiceScopeFactory serviceScopeFactory;
 
-        public LastValues(TermoHubContext context)
+        public LastValues(IServiceScopeFactory serviceScopeFactory)
         {
-            cache = new ConcurrentDictionary<int, IDictionary<int, double>>();
-            LoadFromDatabase(context);
-        }
-
-        private void LoadFromDatabase(TermoHubContext context)
-        {
-            foreach (var sensor in context.Sensors)
-            {
-                context.Entry(sensor).Collection(s => s.Readings).Load();
-                double last = sensor.Readings
-                    .OrderByDescending(r => r.Time)
-                    .Select(r => r.Value)
-                    .FirstOrDefault();
-                SetSensorLastValue(sensor.DeviceId, sensor.SensorId, last);
-            }
-        }
-
-        public double GetDeviceLastValuesAverage(int deviceId)
-        {
-            if (cache.ContainsKey(deviceId) && cache[deviceId].Values.Count > 0)
-            {
-                return cache[deviceId].Values.Average();
-            }
-            return double.NaN;
+            cache = new ConcurrentDictionary<(int, int), double>();
+            this.serviceScopeFactory = serviceScopeFactory;
         }
 
         public double GetSensorLastValue(int deviceId, int sensorId)
         {
-            if (cache.ContainsKey(deviceId) && cache[deviceId].ContainsKey(sensorId))
+            if (!cache.TryGetValue((deviceId, sensorId), out double value))
             {
-                return cache[deviceId][sensorId];
+                using (var scope = serviceScopeFactory.CreateScope())
+                using (var context = scope.ServiceProvider.GetRequiredService<TermoHubContext>())
+                {
+                    value = context.Readings
+                        .Where(r => r.DeviceId == deviceId)
+                        .Where(r => r.SensorId == sensorId)
+                        .OrderByDescending(r => r.Time)
+                        .Select(r => r.Value)
+                        .FirstOrDefault();
+                    SetSensorLastValue(deviceId, sensorId, value);
+                }
             }
-            return double.NaN;
+            return value;
         }
 
         public void SetSensorLastValue(int deviceId, int sensorId, double value)
         {
-            if (!cache.ContainsKey(deviceId))
-            {
-                cache[deviceId] = new ConcurrentDictionary<int, double>();
-            }
-            cache[deviceId][sensorId] = value;
+            cache[(deviceId, sensorId)] = value;
         }
     }
 }
